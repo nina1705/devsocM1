eeb4833381f16d9a31404546a36b2d05a6290aba
nina1705 <anannyaaggarwal2005@gmail.com>
2024-02-19 18:03:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.querySelector('.value');
+
+function showthevalue(value) {
+    display.value += value;
+}
+
+function clearall() {
+    display.value = '';
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
